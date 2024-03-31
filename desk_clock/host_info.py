d72d15e1e1e0d334796905352baa6bfd0c1f88e4
Andrew Lin <andrew.lin@ieee.org>
2024-03-30 19:23:05 -0700

    Improved layout

--- desk_clock/host_info.py
+++ desk_clock/host_info.py
@@ -1,8 +1,60 @@
 import socket
+import subprocess
 
 def host_info():
     hostname = socket.gethostname()
-    ipv4 = socket.gethostbyname(hostname)
-    ipv6 = 'TBD'
+    ipv4 = get_ipv4()
+    ipv6 = get_ipv6()
+    ssid = get_ssid()
+    other_info = get_other_info()
 
-    return f'hostname: {hostname}\nIPv4: {ipv4}\nIPv6: {ipv6}'
+    return (
+        f'hostname: {hostname} || IPv4: {ipv4} || IPv6: {ipv6}\n'
+        f'SSID: {ssid}'
+        f'{other_info}'
+    )
+
+
+def get_ipv4():
+    cmd = ['ip', 'a']
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    return inet_addrs(result.stdout, 'inet', verboten='127')
+
+
+def get_ipv6():
+    cmd = ['ip', 'a']
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    return inet_addrs(result.stdout, 'inet6', verboten='::')
+
+
+def get_ssid():
+    cmd = ['iwgetid', '-r']
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    return result.stdout
+
+
+def get_other_info():
+    try:
+        with open('other_info.txt', 'r') as f:
+            info = ''.join(f.readlines())
+    except FileNotFoundError:
+        info = ''
+
+    return info
+
+
+def inet_addrs(output, inet_token, verboten):
+    tokens = output.split()
+    inet_indexes = []
+    for idx, token in enumerate(tokens):
+        if token == inet_token:
+            inet_indexes.append(idx + 1)
+
+    ip_addrs = [
+        tokens[idx].split('/')[0] 
+        for idx in inet_indexes
+        if not tokens[idx].startswith(verboten)
+    ]
+
+    return ', '.join(ip_addrs)
+
--- deskclock.py
+++ deskclock.py
@@ -1,5 +1,8 @@
+import argparse
+
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 
 from kivy.uix.widget import Widget
@@ -20,13 +23,30 @@ class DeskClock(GridLayout):
     def update(self, dt):
         self.chronometer.update(dt)
 
+
 class DeskClockApp(App):
     def build(self):
         desk_clock = DeskClock()
         desk_clock.build()
         Clock.schedule_interval(desk_clock.update, 0.5)
         return desk_clock
-        
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--show-cursor', 
+        action='store_true', 
+        help='Show cursor in window (useful for non-touchscreens)'
+    )
+
+    args = parser.parse_args()
+    return args
     
-if __name__ == '__main__':
+def main():
+    args = parse_command_line()
+    Window.show_cursor = args.show_cursor
     DeskClockApp().run()
+
+if __name__ == '__main__':
+    main()
