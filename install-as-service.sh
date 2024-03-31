1b217199dcd07e547871f09d54297d78648de1d1
Andrew Lin <andrew.lin@ieee.org>
2024-03-31 12:14:36 -0700

    Systemd service to start on boot

--- install-as-service.sh
+++ install-as-service.sh
@@ -5,8 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SYSTEMD_USER_DIR="${HOME}/.config/systemd/user"
 SERVICE="deskclock.service"
 
-mkdir -p {SYSTEMD_USER_DIR}
+mkdir -p ${SYSTEMD_USER_DIR}
 cp ${SCRIPT_DIR}/${SERVICE} ${SYSTEMD_USER_DIR}/${SERVICE}
 systemctl --user daemon-reload
 systemctl --user enable ${SERVICE}
 echo "${SERVICE} is $(systemctl --user is-enabled ${SERVICE})"
+loginctl enable-linger pi
